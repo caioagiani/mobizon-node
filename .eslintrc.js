5f46bb3aa4b67e5f9a5818b53c74397d10c41b84
caioagiani <agianicaio@gmail.com>
2021-02-12 11:33:00 -0300

    init: configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier', 'import-helpers'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    radix: 0,
+    eqeqeq: 0,
+    camelcase: 0,
+    'no-undef': 0,
+    'no-case-declarations': 'off',
+    'no-inline-comments': 'off',
+    'no-use-before-define': 'warn',
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 0,
+    'no-underscore-dangle': 0,
+    'no-param-reassign': 0,
+    'no-console': 0,
+    'global-require': 0,
+    'consistent-return': 0,
+    'func-names': 0,
+    'no-unused-expressions': 0,
+    'no-const-assign': 0,
+    'no-plusplus': 0,
+    'linebreak-style': 0,
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'no-restricted-syntax': [
+      'warn',
+      {
+        selector: 'ForOfStatement',
+        message: 'frowned upon using For...Of',
+      },
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+  },
+};
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js?(x)'],
+};
