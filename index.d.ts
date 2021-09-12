950c94e61287e28d5e053b767f446aa5efa74cf5
Caio Henrique Agiani <agianicaio@gmail.com>
2021-09-12 19:22:34 -0300

    fix(types): adjust namespace (#41)
    * fix(types): adjust namespace
    * feat(type): add apiVersion optional, default v1
    * chore(package): update version

--- index.d.ts
+++ index.d.ts
@@ -1,82 +1,30 @@
-declare module "mobizon-node" {
-  export const mobizon = {
-    /**
-     * This method is responsible for set the settings.
-     * */
-    setConfig: (data: Config): void => {},
+export declare namespace mobizon {
+  /**
+   * This method is responsible for set the settings.
+   * */
+  function setConfig(data: Config): void;
 
-    /**
-     * Getting of balance information.
-     * */
-    getBalance: (): Promise<Balance> => {},
+  /**
+   * Getting of balance information.
+   * */
+  function getBalance(): Promise<Balance>;
 
-    /**
-     * Sending of a single message.
-     * */
-    sendSms: (data: PayloadSendSMS): Promise<SendSMS> => {},
+  /**
+   * Sending of a single message.
+   * */
+  function  sendSms(data: PayloadSendSMS): Promise<SendSMS>;
+  /**
+   * Getting of SMS message delivery status report.
+   * */
+  function  getSms(data: PayloadGetSMS): Promise<GetSMS>;
+  
+  /**
+   * Getting of the list of SMS messages.
+   * */
+  function  listSms(data: PayloadListSMS): Promise<ListSMS>;
 
-    /**
-     * Getting of SMS message delivery status report.
-     * */
-    getSms: (data: PayloadGetSMS): Promise<GetSMS> => {},
-
-    /**
-     * Getting of the list of SMS messages.
-     * */
-    listSms: (data: PayloadListSMS): Promise<ListSMS> => {},
-  }
-
-  interface Config {
-    apiServer: string
-    /** @default 'v1' */
-    apiKey: 'v1'
-    /** @default 'json' */
-    format: 'json' | 'xml'
-  }
-
-  interface Pagination {
-    pagination: {
-      currentPage: string,
-      pageSize: string,
-    }
-  }
-
-  interface DataBalance {
-    balance: string,
-    /** @default 'BRL' */
-    currency: 'BRL'
-  }
-
-  interface DataSendSMS {
-    campaignId: string,
-    messageId: string,
-    status: number
-  }
-
-  interface PayloadGetSMS {
-    ids: string[]
-  }
-
-  interface Response {
-    code: number, 
-    message?: string 
-  }
-
-  interface Balance extends Response {
-    data: DataBalance, 
-  }
-
-  interface PayloadSendSMS {
-    recipient: string,
-    from: string,
-    text: string
-  }
-
-  interface SendSMS extends Response {
-    data: DataSendSMS,
-  }
-
-  type StatusSMS = 
+  /** StatusSMS */
+    type StatusSMS = 
     'DELIVRD' | 
     'NEW'     | 
     'ENQUEUD' | 
@@ -87,7 +35,68 @@ declare module "mobizon-node" {
     'EXPIRED' | 
     'DELETED'
 
-  interface DataGetSMS {
+  /** Config */
+  export interface Config {
+    apiServer: string
+    apiKey: string
+    /** @default 'json' */
+    format: 'json' | 'xml'
+    /** @default 'v1' */
+    apiVersion?: 'v1'
+  }
+
+  /** Pagination */
+  export interface Pagination {
+    pagination: {
+      currentPage: string,
+      pageSize: string,
+    }
+  }
+
+  /** DataBalance */
+  export interface DataBalance {
+    balance: string,
+    /** @default 'BRL' */
+    currency: 'BRL'
+  }
+  
+  /** DataSendSMS */
+  export interface DataSendSMS {
+    campaignId: string,
+    messageId: string,
+    status: number
+  }
+
+  /** PayloadGetSMS */
+  export interface PayloadGetSMS {
+    ids: string[]
+  }
+
+  /** Response */
+  export interface Response {
+    code: number, 
+    message?: string 
+  }
+
+  /** Balance */
+  export interface Balance extends Response {
+    data: DataBalance, 
+  }
+
+  /** PayloadSendSMS */
+  export interface PayloadSendSMS {
+    recipient: string,
+    from: string,
+    text: string
+  }
+
+  /** SendSMS */
+  export interface SendSMS extends Response {
+    data: DataSendSMS,
+  }
+
+  /** DataGetSMS */
+  export interface DataGetSMS {
     id: string,
     status: StatusSMS,
     segNum: string,
@@ -95,11 +104,13 @@ declare module "mobizon-node" {
     statusUpdateTs: string
   }
 
-  interface GetSMS extends Response {
+  /** GetSMS */
+  export interface GetSMS extends Response {
     data: DataGetSMS[],
   }
 
-  interface PayloadListSMS extends Pagination {
+  /** PayloadListSMS */
+  export interface PayloadListSMS extends Pagination {
     criteria?: {
       from?: string
     },
@@ -108,7 +119,8 @@ declare module "mobizon-node" {
     },
   }
 
-  interface DataListSMS {
+  /** DataListSMS */
+  export interface DataListSMS {
     contentProviderName?: string | unknown | undefined,
     partnerId: string,
     userId: string,
@@ -136,87 +148,11 @@ declare module "mobizon-node" {
     systemCurrency?: string | unknown | undefined,
     campaign: {
       commonStatus: string,
-      counters: {
-        campaignId: string,
-        updateTs: string,
-        totalNewSegNum: string,
-        totalEnqueudSegNum: string,
-        totalAcceptdSegNum: string,
-        totalDelivrdSegNum: string,
-        totalRejectdSegNum: string,
-        totalExpiredSegNum: string,
-        totalUndelivSegNum: string,
-        totalDeletedSegNum: string,
-        totalUnknownSegNum: string,
-        totalPdlivrdSegNum: string,
-        totalSegNum: string,
-        totalNewMsgNum: string,
-        totalEnqueudMsgNum: string,
-        totalAcceptdMsgNum: string,
-        totalDelivrdMsgNum: string,
-        totalRejectdMsgNum: string,
-        totalExpiredMsgNum: string,
-        totalUndelivMsgNum: string,
-        totalDeletedMsgNum: string,
-        totalUnknownMsgNum: string,
-        totalPdlivrdMsgNum: string,
-        totalMsgNum: string,
-        totalCost: string,
-        totalPartnerCost: string,
-        totalNewMsgCost: string,
-        totalEnqueudMsgCost: string,
-        totalAcceptdMsgCost: string,
-        totalDelivrdMsgCost: string,
-        totalRejectdMsgCost: string,
-        totalExpiredMsgCost: string,
-        totalUndelivMsgCost: string,
-        totalDeletedMsgCost: string,
-        totalUnknownMsgCost: string,
-        totalPdlivrdMsgCost: string,
-        partnerCurrency: string,
-        userCurrency: string,
-        recipientsRejected: string
-      },
-      createTs: string,
-      creationWay: string,
-      currency?: string | unknown | undefined,
-      deferredToTs?: string | unknown | undefined,
-      endSendTs: string,
-      expirationTs: string,
-      extra: {
-        validity: string,
-        mclass: string,
-        isTestAlphanameUsed: true,
-        coding: string,
-        charset: string,
-        trackShortLinkRecipients: 0
-      },
-      from: string,
-      globalComment?: string | unknown | undefined,
-      globalModStatus: string,
-      groups?: string | unknown | undefined,
-      groupsList: any[],
-      highlightedText?: string | unknown | undefined,
-      id: string,
-      isTemplateComment?: string | unknown | undefined,
-      moderationStatus: string,
-      msgType: string,
-      name?: string | unknown | undefined,
-      partnerId: string,
-      partnerModStatus: string,
-      rateLimit?: string | unknown | undefined,
-      ratePeriod?: string | unknown | undefined,
-      recipientsSource?: string | unknown | undefined,
-      sendStatus: string,
-      startSendTs: string,
-      text: string,
-      type: string,
-      usedStopListWords?: string | unknown | undefined,
-      userId: string
     }
   }
 
-  interface ListSMS extends Response {
+  /** ListSMS */
+  export interface ListSMS extends Response {
     data: {
       items: DataListSMS[],
       totalItemCount: string
